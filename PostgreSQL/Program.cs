41da73bebb706333156f23c970b70eb36d2ecf7b
OuRongwei <74855272+OuRongwei@users.noreply.github.com>
2022-08-26 17:30:21 +0800

    优化表及查询方法

--- PostgreSQL/Program.cs
+++ PostgreSQL/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Data;
 using System.Diagnostics;
+using System.Threading;
 
 namespace PostgreSQL
 {
@@ -16,108 +17,63 @@ namespace PostgreSQL
             Stopwatch stopwatch = new Stopwatch();
 
             #region 初始化数据库
-            //string initSQL = "delete from t1";
             string initSQL = "delete from test";
             ExecNonQuery(initSQL, conn);
             Console.WriteLine("数据库初始化成功！");
-            //Console.WriteLine();
-            #endregion
-
-            #region 查询数据量
-            //// 查询数据量
-            //var cnt = new NpgsqlCommand("select count(id) from t1", conn);
-            //var readcnt = cnt.ExecuteReader();
-            //if (readcnt.Read())
-            //{
-            //    Console.WriteLine($"数据库共有{readcnt.GetInt32(0)}条数据");
-            //    Console.WriteLine("-------------------------------------------");
-            //    readcnt.Close();
-            //}
             #endregion
 
             Console.WriteLine("\n插入数据：\n");
 
-            #region 插入数据
-            for (int k = 1; k <= 100000; k *= 10)
+            for (int k = 1000; k <= 10000; k *= 10)
             {
-                //DateTime start = DateTime.Now;
-                stopwatch.Restart();
-
-                for (int i = 1; i <= k; i++)
+                for (int j = 1; j <= 5; j++)
                 {
-                    // 插入数据
-                    //string inSQL = "insert into t1 values((random()*10000000)::integer)";
-                    //string inSQL = $"insert into t1 values({i})";
-                    string inSQL = $"insert into test values({k+i})";
-                    ExecNonQuery(inSQL, conn);
-                    //Console.WriteLine("变更行数:" + ExecNonQuery(SQL, conn));
+                    stopwatch.Restart();
+
+                    for (int i = 1; i <= k; i++)
+                    {
+                        // 插入数据
+                        DateTime date = DateTime.UtcNow;
+                        string inSQL = $"insert into test values('{date}',{j * k + i},{j * k})";
+                        ExecNonQuery(inSQL, conn);
+                    }
+                    stopwatch.Stop();
+
+                    Console.WriteLine(string.Format($"插入{k}条数据耗时：{stopwatch.ElapsedMilliseconds} ms"));
+                    Console.WriteLine("-------------------");
+
+                    //string SQL2 = $"select count(id) from test where insert_time > '{DateTime.UtcNow - (DateTime.UtcNow.AddSeconds(10) - DateTime.UtcNow)}'";
+                    //var cnt = new NpgsqlCommand(SQL2, conn);
+                    //var readcnt = cnt.ExecuteReader();
+                    //if (readcnt.Read())
+                    //{
+                    //    Console.WriteLine($"数据库前十秒共有{readcnt.GetInt32(0)}条数据");
+                    //    Console.WriteLine("-------------------------------------------");
+                    //    readcnt.Close();
+                    //}
+
+                    stopwatch.Restart();
+
+                    //Console.WriteLine(DateTime.UtcNow);
+                    //Console.WriteLine(DateTime.UtcNow - (DateTime.UtcNow.AddSeconds(10) - DateTime.UtcNow));
+
+                    string SQL = $"select * from test where insert_time > '{DateTime.UtcNow - (DateTime.UtcNow.AddSeconds(10) - DateTime.UtcNow)}'";
+                    var seSQL = new NpgsqlCommand(SQL, conn);
+
+                    var reader = seSQL.ExecuteReader();
+
+                    while (reader.Read())
+                        reader.GetValue(0);
+                    reader.Close();
+
+                    stopwatch.Stop();
+                    Console.WriteLine(string.Format($"查询{k}条数据耗时：{stopwatch.ElapsedMilliseconds} ms"));
+                    Console.WriteLine("\n***************************************\n");
+
+                    Thread.Sleep(15000);
                 }
-                //DateTime end = DateTime.Now;
-                //TimeSpan abs = end - start;
-                stopwatch.Stop();
-
-                //Console.WriteLine(string.Format($"程序执行时间：{abs.TotalMilliseconds}ms"));
-                Console.WriteLine(string.Format($"插入{k}条数据耗时：{stopwatch.ElapsedMilliseconds} ms"));
-                Console.WriteLine(string.Format($"插入{k}条数据耗时：{stopwatch.ElapsedTicks} * 10^-4 ms"));
-                Console.WriteLine("-------------------");
+                Console.WriteLine();
             }
-
-            //stopwatch.Restart();
-
-            //for (int i = 1; i <= 1000; i++)
-            //{
-            //    // 插入数据
-            //    string inSQL = $"insert into test values({i})";
-            //    ExecNonQuery(inSQL, conn);
-            //}
-            //stopwatch.Stop();
-            //Console.WriteLine(string.Format($"插入1000条数据耗时：{stopwatch.ElapsedMilliseconds} ms"));
-            //Console.WriteLine(string.Format($"插入1000条数据耗时：{stopwatch.ElapsedTicks} * 10^-4 ms"));
-            //Console.WriteLine("-------------------");
-            #endregion
-
-            Console.WriteLine("\n***************************************\n\n查询数据：\n");
-
-            #region 查询数据
-
-            for (int k = 1; k <= 10; ++k)
-            {
-                // 提前建立连接
-                var tSQL = new NpgsqlCommand("select * from test where id = 1", conn);
-                var tReader = tSQL.ExecuteReader();
-                if (tReader.Read())
-                    tReader.GetInt32(0);
-                tReader.Close();
-
-                //DateTime start = DateTime.Now;
-                stopwatch.Restart();
-
-                var seSQL = new NpgsqlCommand($"select * from t1 limit 1000", conn);
-                var reader = seSQL.ExecuteReader();
-                while (reader.Read())
-                    reader.GetInt32(0);
-                reader.Close();
-
-                //for (int i = 1; i <= 1000; i++)
-                //{
-                //    var seSQL = new NpgsqlCommand($"select * from test where id = {i}", conn);
-                //    var reader = seSQL.ExecuteReader();
-
-                //    if (reader.Read())
-                //        reader.GetInt32(0);
-                //    reader.Close();
-                //}
-
-                //DateTime end = DateTime.Now;
-                //TimeSpan abs = end - start;
-                //Console.WriteLine(string.Format($"程序执行时间：{abs.TotalMilliseconds}ms"));
-
-                stopwatch.Stop();
-                Console.WriteLine(string.Format($"第{k}次查询1000条数据耗时：{stopwatch.ElapsedMilliseconds} ms"));
-                Console.WriteLine(string.Format($"第{k}次查询1000条数据耗时：{stopwatch.ElapsedTicks} * 10^-4 ms"));
-                Console.WriteLine("-------------------");
-            }
-            #endregion
 
             Console.WriteLine("Hello PostgreSQL End!");
             Console.ReadLine();
